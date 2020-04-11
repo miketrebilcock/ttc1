502cc3257af46d916fb2f430c156a561364ce3ef
Mackenzie <mackenzie@users-iMac-2.broadband>
2020-04-11 16:44:59 +0100

    train controller host

---
+++ TrainController5000/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+const trains = {}
+function trainPing(request, response) {
+    console.log('recieved', request.body)
+    trains[request.body.macAddress] = request.body
+    response.send('ok')
+}
+
+function getTrains(request, response) {
+    response.send(trains)
+}
+app.put('/api/train', trainPing)
+app.get('/api/trains', getTrains)
+app.use(express.static('../MobileController'))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
